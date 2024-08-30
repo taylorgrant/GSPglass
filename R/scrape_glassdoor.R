bbc326f4ae2f1b7e9f5b30905e5f8c018223bd82
taylorgrant <taylorgrant38@gmail.com>
2024-08-30 15:41:18 -0700

    pull from site script rather than rvest

--- R/scrape_glassdoor.R
+++ R/scrape_glassdoor.R
@@ -2,6 +2,7 @@
 #'
 #' @description Function to scrape Glassdoor reviews and put into a tidy format
 #'
+#' @param corp Company name from url - With the url, can also run `corp_name(url)$hyphen_name` to get it
 #' @param companyID String with alpha-numeric company ID
 #' @param max Maximum number of pages to scrape
 #'
@@ -10,85 +11,108 @@
 #'
 #' @examples
 #' \dontrun{
-#' reviews <- scrape_glassdoor("E14069", max)
+#' reviews <- scrape_glassdoor("State-Farm," E2990", max)
 #' }
-scrape_glassdoor <- function(companyID, max) {
+scrape_glassdoor <- function(corp, companyID, max) {
   Sys.sleep(3)
-  # add headers
-  headers = c(
-    `sec-ch-ua` = '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-    `Referer` = "https://www.glassdoor.com/",
-    `DNT` = "1",
-    `Accept-Language` = "en",
-    `sec-ch-ua-mobile` = "?0",
-    `User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-    `sec-ch-ua-platform` = '"macOS"'
-  )
   # scrape and put into dataframe
   pb <- progress::progress_bar$new(total = max)
   get_reviews <- function(pg){
+
+    # extract employer reviews
+    expand_employer_review <- function(lst) {
+      expanded_lst <- lapply(names(lst), function(name) {
+        if (grepl("^EmployerReviewRG", name)) {
+          return(lst[[name]])
+        } else {
+          return(NULL)
+        }
+      })
+      expanded_lst <- Filter(Negate(is.null), expanded_lst) # Remove NULL values
+      return(expanded_lst)
+    }
+    # employer name from id
+    get_employer_name <- function(empID, nested_list) {
+      if (!is.null(empID)) {
+        text <- nested_list$props$pageProps$apolloCache[[empID]]$shortName
+      } else {
+        text <- NA
+      }
+      return(text)
+    }
+    # employee job title from id
+    get_jobtitle_text <- function(jobID, nested_list) {
+      if (!is.null(jobID)) {
+        text <- nested_list$props$pageProps$apolloCache[[jobID]]$text
+      } else {
+        text <- NA
+      }
+      return(text)
+    }
+    # employee location from id
+    get_location_text <- function(locID, nested_list) {
+      if (!is.null(locID)) {
+        text <- nested_list$props$pageProps$apolloCache[[locID]]$name
+      } else {
+        text <- NA
+      }
+      return(text)
+    }
+
     pb$tick()
     Sys.sleep(3)
     tryCatch({
 
-      start_url <- "https://www.glassdoor.com/Reviews/Company-Reviews-"
-      settings_url <- ".htm?sort.sortType=RD&sort.ascending=false&filter.iso3Language=eng"
-      # pg_reviews <- rvest::session(paste0(start_url, companyID, "_P", pg, settings_url))
-      pg_reviews <- httr::content(httr::GET(url = paste0(start_url, companyID, "_P", pg, settings_url),
-                                            httr::add_headers(.headers = headers)))
-      data.frame(date = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__reviewDate") |>
-                   rvest::html_text2(),
+      # add headers
+      headers = c(
+        `sec-ch-ua` = '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        `Referer` = "https://www.glassdoor.com/",
+        `DNT` = "1",
+        `Accept-Language` = "en",
+        `sec-ch-ua-mobile` = "?0",
+        `User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+        `sec-ch-ua-platform` = '"macOS"'
+      )
 
-                 title = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__employee") |>
-                   rvest::html_text2(),
+      settings_url <- "?sort.sortType=RD&sort.ascending=false&filter.iso3Language=eng"
+      url <- glue::glue("https://www.glassdoor.com/Reviews/{corp}-Reviews-{companyID}_P{pg}.htm{settings_url}")
 
-                 summary = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__title") |>
-                   rvest::html_text(),
+      pg_reviews <- httr::content(httr::GET(url,httr::add_headers(.headers = headers)))
+      # pulling data from the JS script
+      tmp <- pg_reviews |>
+        rvest::html_elements("script#__NEXT_DATA__") |>
+        rvest::html_text() |>
+        jsonlite::fromJSON()
 
-                 rating = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__overallRating") |>
-                   rvest::html_text(),
+      # expand reviews and put list to dataframe
+      out <- expand_employer_review(tmp$props$pageProps$apolloCache) |>
+        data.table::rbindlist() |>
+        tibble()
 
-                 employee_type = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__employeeDetails:nth-child(1)") |>
-                   rvest::html_text(),
+      # clean and extract data
+      out |>
+        dplyr::rowwise() |>
+        dplyr::mutate(employer = get_employer_name(employer, tmp),
+               jobTitle = get_jobtitle_text(jobTitle, tmp),
+               location = get_location_text(location, tmp),
+               lengthOfEmployment = dplyr::case_when(lengthOfEmployment == 0 ~ NA,
+                                              lengthOfEmployment == 1 ~ "Less than 1 year",
+                                              TRUE ~ glue::glue("More than {lengthOfEmployment-1} years")),
+               review_date = as.Date(lubridate::as_datetime(reviewDateTime)),
+               status = ifelse(isCurrentJob == "TRUE", "current_employee", "former_employee"),
+               dplyr::across(c(advice, cons, pros, summary), ~stringr::str_replace_all(., "\n", "; ")),
+               dplyr::across(c(advice, cons, pros, summary), ~stringr::str_replace_all(., "&amp;", "&"))) |>
+        dplyr::select(employer, review_date, title = jobTitle, status, duration = lengthOfEmployment,
+               rating = ratingOverall, summary, pros, cons, advice,
+               ratingBusinessOutlook:ratingWorkLifeBalance
+        ) |>
+        dplyr::mutate(across(summary:cons, trimws))
 
-                 pros = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__pro span") |>
-                   rvest::html_text(),
-
-                 cons = pg_reviews |>
-                   rvest::html_elements(".review-details__review-details-module__con span") |>
-                   rvest::html_text()
-
-      )}, error = function(e){
+      }, error = function(e){
         NULL
       })
   }
   cat("Pulling all reviews now...\n")
   out <- 1:max |>
     purrr::map_dfr(get_reviews)
-
-  # 3. tidy the reviews and get in order
-  tidy_reviews <- function(tbl) {
-    tbl |>
-      dplyr::mutate(review_date = as.Date(trimws(gsub("\\-.*", "", date)), format = "%b %d, %Y"),
-                    title = trimws(title),
-                    rating = as.numeric(stringr::str_replace_all(rating, ".0", "")),
-                    status = gsub("\\,.*", "", employee_type),
-                    duration = trimws(ifelse(stringr::str_detect(employee_type, ","),
-                                             gsub(".*\\,", "", employee_type),
-                                             NA_character_))) |>
-      dplyr::select(-c(date, employee_type)) |>
-      dplyr::relocate(review_date, .before = dplyr::everything()) |>
-      dplyr::relocate(title, .after = "review_date") |>
-      dplyr::relocate(status, .after = "title") |>
-      dplyr::relocate(duration, .after = "status") |>
-      dplyr::arrange(dplyr::desc(review_date))
-  }
-  cat("Cleaning up and parsing...\n")
-  out_tidy <- tidy_reviews(out)
 }
